9a557e222bff805e492d42bc6b287f9292e11852
Riteshgarna12 <riteshgarna12@gmail.com>
2025-06-24 14:35:00 +0530

    add div  in app

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
         <h1 className="bg-amber-900 text-amber-50 p-2 text-center text-3xl font-bold underline">
           Hello world!
         </h1>
+        <div className='w-full bg-pink-200 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores porro hic, quas tempora doloremque suscipit animi quos, error voluptate unde nesciunt. Porro saepe dolore repudiandae assumenda fugit nihil maiores dolor!</div>
     </>
   )
 }
