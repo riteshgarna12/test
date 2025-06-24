c0675d4fac965e6f4dedf7907a0bd8d13581132a
Riteshgarna12 <riteshgarna12@gmail.com>
2025-06-24 14:29:55 +0530

    next

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1 className="bg-amber-900 text-amber-50 p-2 text-center text-3xl font-bold underline">
+          Hello world!
+        </h1>
+    </>
+  )
+}
+
+export default App
